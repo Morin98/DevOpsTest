c67855b777838b951092383ed8a7d7164fd521aa
Ruben Morin <99023796+Morin98@users.noreply.github.com>
2024-12-19 14:56:26 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 import datetime
+import os
 
 minute = datetime.datetime.now().minute
 if minute % 2 == 0:
     print('Current minute is even')
+    os.exit(0)
 else:
     print('Current minute is odd')
+    os.exit(1)
